1fd262bd5368be98e7358d6a97d87716e2f4b135
Simon Søndergaard <simon.sondergaard@xamarin.com>
2016-09-14 13:05:45 +0200

    more conversions

---
+++ sample-code/examples/java/junit/AndroidApiDemos.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+rm target/upload/test-classes/com/saucelabs/appium/AndroidContactsTest.class
+rm target/upload/test-classes/com/saucelabs/appium/iOSPageObjectTest.class
+rm target/upload/test-classes/com/saucelabs/appium/SafariTest.class
+rm target/upload/test-classes/com/saucelabs/appium/SauceTest.class
+rm target/upload/test-classes/com/saucelabs/appium/SimpleTest.class
+rm target/upload/test-classes/com/saucelabs/appium/UICatalogTest.class
+rm target/upload/test-classes/com/saucelabs/appium/AndroidWebViewTest.class
+find target/upload/test-classes/com/saucelabs/appium/page_object -name "*Test*" -exec rm {} \;
+
+
